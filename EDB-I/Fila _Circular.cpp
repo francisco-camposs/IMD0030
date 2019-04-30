c9ad8953e6e32c94c9b732898c46825b7ce3d2fb
Francisco De Assis Campos Junior <juniorcampos@A204-02L.imd.local>
2019-04-30 12:56:39 -0300

    Implementação de estrutura de dados de EDB-I versão 2

--- EDB-I/Fila _Circular.cpp
+++ EDB-I/Fila _Circular.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 typedef struct Fila{
+	int inicio;
 	int N;
 	int N_Max;
 	int *Dado;
@@ -16,7 +17,18 @@ Fila Criar(int N){
 	return heap;
 }
 
-
+void inserir(Fila * VV; int Valor){
+	if (VV->N == VV->N_max){
+		cout << "ERROR 00X00: \n Não há espaço para inserir." << endl;
+		return;
+	}
+	if (VV->inicio - 1 < 0){
+		VV->inicio += VV->N_Max;
+	}
+	VV->inicio -= 1;
+	VV->Dado[VV->inicio] = valor;
+	VV->N += 1;
+}
 
 
 int main (){
